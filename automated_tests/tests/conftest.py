c5a252229e286de5139175699f5402b3b896a4c5
sofiazlobina <zlobinasophya@gmail.com>
2026-05-30 11:43:16 +0300

    Update conftest.py

--- automated_tests/tests/conftest.py
+++ automated_tests/tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
+
+BASE_URL = os.getenv("APP_BASE_URL", "http://localhost:8000")
 
 @pytest.fixture(scope="session")
 def browser():
@@ -8,6 +11,7 @@ def browser():
     edge_options.add_argument("--headless")
     edge_options.add_argument("--no-sandbox")
     edge_options.add_argument("--disable-dev-shm-usage")
+    edge_options.add_argument("--disable-gpu") 
     
     driver = webdriver.Edge(options=edge_options)
     yield driver
